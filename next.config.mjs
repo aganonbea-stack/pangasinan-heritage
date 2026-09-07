b571dddd1d9f975e3b55a4dec5399d7646c0791d
Bea Aganon <aganonbea@gmail.com>
2026-09-08 00:02:58 +0800

    Fix GitHub Pages asset paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
   basePath: isGithubActions ? "/pangasinan-heritage" : "",
+
   assetPrefix: isGithubActions ? "/pangasinan-heritage/" : "",
+
   trailingSlash: true,
 };
 
